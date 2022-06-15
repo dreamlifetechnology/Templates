ae7a723cdb3c7bc3ea17fc549db554a768f93fd6
Juan <juan.roncero@gmail.com>
2022-06-15 11:59:49 +0200

    Patterns

---
+++ PatternDesigns/Patrones de diseño creacionales/6. Adapter/AdapterPattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternDesigns.Patrones_de_diseño_creacionales._6._Adapter
+{
+    // Trata de comunicar interfaces distintas para que puedan interactuar entre sí.
+    public class AdapterPattern
+    {
+        public void init()
+        {
+
+        }
+
+
+    }
+    public static class ProductDataProvider
+    {
+        public static List<Product1> GetData() =>
+            new List<Product1>
+            {
+                new Product1("Android 1.1","500"),
+                new Product1("iPhone 10","1500"),
+                new Product1("Android 1.5","700")
+            };
+    }
+
+    public class Product1
+    {
+        public Product1(string name, string price)
+        {
+            Name = name;
+            Price = price;        
+        }
+
+        public string Name{ get; set; }
+        public string Price{ get; set; }
+
+    }
+
+}
